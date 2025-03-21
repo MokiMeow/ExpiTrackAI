b1fff7107b6a10d33dfea0b14bc8cfd0c61afba0
MokiMeow <122464960+MokiMeow@users.noreply.github.com>
2025-03-21 20:05:49 +0530

    lets commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   base: "./",
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "dist/public"), // ← static goes here
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
   resolve: {
